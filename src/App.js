25706d4d95f7037262f83f2af8fca0abbe606d3b
“Foxshane997” <Foxshane997@gmail.com>
2024-06-03 21:40:22 -0700

    Added input & submit fields

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React, { useState } from 'react'
 import './App.css';
 
 import ColorBlock from './ColorBlock';
+import ColorForm from './ColorForm';
 
 function App(){
-    let colors = ['violet', 
-    'blue', 'lightblue', 
-    'green', 'greenyellow', 
-    'yellow', 'orange', 'red']
+    let [colors, setColors] = useState
+    ([
+        'violet', 'blue',
+        'lightblue', 'green',
+        'greenyellow', 'yellow',
+        'orange', 'red'
+    ])
 
     let colorMap = colors.map((color, i) => {
       return (
@@ -17,9 +21,12 @@ function App(){
     })
 
     return (
-        <div className="App">
-            {colorMap}
-        </div>
+      <div className="App">
+        {colors.map((color, i) => 
+          <ColorBlock key={i} color={color} />
+        )}
+        <ColorForm/>
+      </div>  
     )
 }
 
