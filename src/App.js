af24ead03748a20e141cc68f0b7bfb1a5e7428a4
“Foxshane997” <Foxshane997@gmail.com>
2024-06-03 21:13:45 -0700

    Added ColorBlock component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React, { useState } from 'react'
+import './App.css';
+
+function App(){
+    let colors = ['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']
+    return (
+        <div className="App">
+
+        </div>
+    )
+}
+
+export default App;
